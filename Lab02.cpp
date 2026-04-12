0b4e72fb802043055e58f2e2bba6d429b686c185
DanielChris-NewDev <hogiaphuca@gmail.com>
2026-04-12 14:18:34 +0700

    Added part 2 & 3

--- Lab02.cpp
+++ Lab02.cpp
@@ -1,4 +1,4 @@
-﻿#include "Lab02.h"
+#include "Lab02.h"
 
 void consoleWindow() {
 	HWND consoleWindow = GetConsoleWindow();
@@ -47,6 +47,177 @@ void printBootSectorInfo(bootSector bs){
     cout << "| " << setw(35) << "Number of sectors for the RDET" << "| " << setw(15) << rdetSectors << " |" << endl;
     cout << "| " << setw(35) << "Total number of sectors on the disk" << "| " << setw(15) << bs.totalSectors32 << " |" << endl;
     cout << "-------------------------------------------------------\n";
+}
+
+vector<ENTRY> searchFiles(HANDLE usb, const bootSector& bs, UINT32 startCluster) {
+    vector<ENTRY> txtfiles;
+
+    DWORD dataSection = (bs.reservedSectorNum + bs.numOfFATTable * bs.FAT32.tableSize_32) * bs.bytesPerSector;
+    //Skip boot sector and FAT, bắt đầu ở byte đầu tiên của vùng data 
+
+    DWORD firstCluster = (startCluster - 2) * bs.sectorsPerCluster * bs.bytesPerSector;
+    //Cluster đầu tiên (cluster thứ 2) của vùng data (bỏ cluster 0 và 1)
+
+    DWORD startByte = dataSection + firstCluster;
+
+    SetFilePointer(usb, startByte, NULL, FILE_BEGIN);
+    // Bắt đầu ở sector đầu tiên
+
+    for (int s = 0; s < bs.sectorsPerCluster; s++) {
+        BYTE sector[512]; //các byte được đọc
+        DWORD bytes_read; // các byte đã đọc
+
+        if (ReadFile(usb, sector, 512, &bytes_read, NULL)) {// Bat91 đầu đọc data
+            for (int i = 0; i < 512; i += 32) {
+                if (sector[i] == 0x00) return txtfiles; //rỗng
+                if (sector[i] == 0xE5) continue; //file bị xóa
+
+                string dataType = "";
+                dataType += sector[i + 8]; //0x08 là byte thứ 9
+                dataType += sector[i + 9];
+                dataType += sector[i + 10];
+
+                if (dataType == "TXT") {
+                    ENTRY file;
+                    char* temp = (char*)&sector[i]; // trỏ tới điểm đầu của entry
+                    file.name = string(temp, 8); //tên file là 8 byte đầu tiên
+
+                    memcpy(&file.fileSize, &sector[i + 28], 4);// 0x1C là byte thứ 29
+
+                    UINT16 date;
+                    UINT32 time;
+                    memcpy(&date, &sector[i + 16], 2); // 0x10 là byte thứ 17
+                    //Đọc từ phải sang trái thay vì sử dụng endian
+                    int day = date & 0x1F; //Chỉ đọc 5 bit cuối (chuyển bit khác thành 0)
+                    int month = (date >> 5) & 0x0F; // Chỉ đọc 4 bit cuối
+                    int year = (date >> 9) + 1980; //Đọc 7 bit còn lại
+                    file.dateCreated = to_string(day) + "/" + to_string(month) + "/" + to_string(year);
+
+                    memcpy(&time, &sector[i + 13], 3); // 0x0E là byte thứ 14
+                    int milisecond = time & 0x7F; //đọc 7 bit
+                    int second = (time >> 7) & 0x3F;//đọc 6 bit
+                    int minute = (time >> 13) & 0x3F; //đọc 6 bit
+                    int hour = (time >> 19) & 0x1F; //đọc 5 bit
+                    file.timeCreated = to_string(hour) + ":" + to_string(minute) + ":" + to_string(second) + "." + to_string(milisecond);
+
+                    UINT16 highByte;
+                    memcpy(&highByte, &sector[i + 20], 2); //0x14 là byte thứ 21
+                    UINT16 lowByte;
+                    memcpy(&lowByte, &sector[i + 26], 2); // 0x1A là byte thứ 27
+
+                    file.startCluster = (highByte << 16) | lowByte; // Little endian
+
+                    txtfiles.push_back(file);
+                }
+                //Kiểm tra subfolders
+                if (sector[i + 11] & 0x10) { //Lấy bit thứ 4 của offset 0x0B
+                    if (sector[i] == '.') continue;
+
+                    UINT16 highByte;
+                    memcpy(&highByte, &sector[i + 20], 2);
+                    UINT16 lowByte;
+                    memcpy(&lowByte, &sector[i + 26], 2);
+
+                    UINT32 subFolderCluster = (highByte << 16) | lowByte;
+                    vector<ENTRY> subFolder = searchFiles(usb, bs, subFolderCluster);
+                    for (const auto& file : subFolder) {
+                        txtfiles.push_back(file);
+                    }
+                    SetFilePointer(usb, startByte + (s * 512) + (i + 32), NULL, FILE_BEGIN);
+                    //trỏ tới sector của entry tiếp theo
+                }
+            }
+        }
+    }
+    //CloseHandle(usb);
+    return txtfiles;
+}
+
+string fileContent(HANDLE usb, const bootSector& bs, const ENTRY& file) {
+    DWORD dataSection = (bs.reservedSectorNum + bs.numOfFATTable * bs.FAT32.tableSize_32) * bs.bytesPerSector;
+    DWORD firstCluster = (file.startCluster - 2) * bs.sectorsPerCluster * bs.bytesPerSector;
+    // Cluster bắt đầu của file
+
+    DWORD startByte = dataSection + firstCluster;
+    SetFilePointer(usb, startByte, NULL, FILE_BEGIN);
+
+    // Bắt đầu ở sector đầu tiên
+    BYTE buffer[512];
+    DWORD bytes_read; // các byte đã đọc
+
+    if (!ReadFile(usb, buffer, 512, &bytes_read, NULL)) { //bắt đầu từ phần tử/chữ đầu tiên trong file
+        cout << "Unable to read file's information\n";
+        return "";
+    }
+
+    return string((char*)buffer, file.fileSize);
+}
+
+void readFileContent(string file) {
+    stringstream ss(file);
+    int queueAmount;
+
+    ss >> queueAmount;
+
+    vector<Queue> queues;
+    for (int i = 0; i < queueAmount; i++) {
+        Queue q;
+        ss >> q.QID >> q.timeSlice >> q.schedulingPolicy;
+        queues.push_back(q);
+    }
+
+    vector<Process> processes;
+    Process p;
+
+    while (ss >> p.PID >> p.arrivalTime >> p.burstTime >> p.queueID) {
+        p.remainingTime = p.burstTime;
+        processes.push_back(p);
+    }
+
+    cout << left << setw(20) << " ";
+    for (const auto& process : processes) cout << setw(15) << process.PID;
+    cout << endl;
+
+    cout << left << setw(20) << "Arrival time";
+    for (const auto& process : processes) cout << setw(15) << process.arrivalTime;
+    cout << endl;
+
+    cout << left << setw(20) << "Burst time";
+    for (const auto& process : processes) cout << setw(15) << process.burstTime;
+    cout << endl;
+
+    cout << left << setw(20) << "Queue ID";
+    for (const auto& process : processes) cout << setw(15) << process.queueID;
+    cout << endl;
+
+    cout << left << setw(20) << "Time slice";
+    for (const auto& process : processes) {
+        int slice = 0;
+        for (const auto& q : queues) if (q.QID == process.queueID) slice = q.timeSlice;
+        cout << setw(15) << slice;
+    }
+    cout << endl;
+
+    cout << left << setw(20) << "Algorithm";
+    for (const auto& process : processes) {
+        string algorithm = "";
+        for (const auto& q : queues) if (q.QID == process.queueID) algorithm = q.schedulingPolicy;
+        cout << setw(15) << algorithm;
+    }
+}
+
+void fileInfo(HANDLE usb, const bootSector& bs, const ENTRY& file) {
+
+    cout << "File name: " << file.name << ".txt" << '\n'
+        << "Date created: " << file.dateCreated << '\n'
+        << "Time created: " << file.timeCreated << '\n'
+        << "Size: " << file.fileSize << " Bytes" << '\n';
+    cout << "== Process Information Table ==\n";
+    string content = fileContent(usb, bs, file);
+    if (content.empty()) {
+        cout << "File is empty\n";
+    }
+    readFileContent(content);
 }
 
 int handleChoice(sf::RenderWindow& window, sf::Font& font, const vector<string>& choices) {
